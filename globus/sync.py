d2063b530ad80f3de689cc93c7c0a39efadf483a
Jesse London <jesselondon@gmail.com>
2013-06-18 23:54:14 -0500

    fixed issue in globus sync with remote path calculation (+cleanup)

--- globus/sync.py
+++ globus/sync.py
@@ -17,6 +17,10 @@ SYNC_LEVEL = 3
 
 def watch(username, token, local_name, local_path,
           remote_name, remote_path, wait):
+    """Sync `local_path` on `local_name` with `remote_path` on `remote_name`
+    every `wait` seconds, using the Globus API.
+
+    """
     while True:
         perform_sync(username, token, local_name, local_path,
                      remote_name, remote_path)
@@ -24,20 +28,23 @@ def watch(username, token, local_name, local_path,
 
 
 def perform_sync(username, token, local_name, local_path,
-                 remote_name, remote_path):
-    files = Files.connect(DB_NAME, TABLE_NAME_FILES)
-    dirs = Dirs(files.connection, TABLE_NAME_DIRS)
+                 remote_name, remote_path, timeout=120):
+    """Sync `local_path` on `local_name` with `remote_path` on `remote_name`,
+    using the Globus API.
+
+    """
+    # Initialize local record of filesystem:
+    connection = FSTable.get_connection(DB_NAME)
+    files = Files(connection, TABLE_NAME_FILES)
+    dirs = Dirs(connection, TABLE_NAME_DIRS)
+
+    # Initialize API client:
     api = api_client.TransferAPIClient(username, goauth=token)
     api.endpoint_autoactivate(local_name)
     api.endpoint_autoactivate(remote_name)
 
-    # TODO: create remote_path on remote machine?
-    try:
-        api.endpoint_ls(remote_name, remote_path)
-    except api_client.ClientError:
-        api.endpoint_mkdir(remote_name, remote_path)
-
-    deadline = datetime.datetime.utcnow() + datetime.timedelta(minutes=2)
+    # Initialize Transfer and Delete sync lists:
+    deadline = datetime.datetime.utcnow() + datetime.timedelta(seconds=timeout)
     _code, _message, data = api.submission_id()
     xfer_submission_id = data['value']
     xfer = api_client.Transfer(
@@ -50,12 +57,13 @@ def perform_sync(username, token, local_name, local_path,
     delete = api_client.Delete(del_submission_id, remote_name, deadline,
                                recursive=True)
 
-    new_dirpaths = []
+    new_dirpaths = [] # New directories to be added recursively
     abspath = os.path.abspath(local_path)
     for dirpath, dirnames, filenames in os.walk(abspath):
-        recursively_included = any(dirpath.startswith(new_dirpath)
+        recursively_included = any(is_ancestor(new_dirpath, dirpath)
                                    for new_dirpath in new_dirpaths)
 
+        # Check for directories removed from this level:
         for expected_dir in dirs.under(dirpath):
             if os.path.basename(expected_dir) not in dirnames:
                 # Directory removed. Recursively delete:
@@ -65,6 +73,7 @@ def perform_sync(username, token, local_name, local_path,
                     remote_path, abspath, expected_dir)
                 delete.add_item(remote_dirpath)
 
+        # Check for directories added to this level:
         for dirname in dirnames:
             dirname_path = os.path.join(dirpath, dirname)
             try:
@@ -80,6 +89,7 @@ def perform_sync(username, token, local_name, local_path,
                 # Record directory for future walks:
                 dirs.add(dirname_path)
 
+        # Check for files removed from this level:
         for expected_file in files.under(dirpath):
             if os.path.basename(expected_file.path) not in filenames:
                 # File removed.
@@ -88,6 +98,7 @@ def perform_sync(username, token, local_name, local_path,
                     remote_path, abspath, expected_file.path)
                 delete.add_item(remote_filepath)
 
+        # Check for files added to or updated at this level:
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
             norm_filepath = filepath.rstrip(os.path.sep)
@@ -111,12 +122,14 @@ def perform_sync(username, token, local_name, local_path,
                     remote_path, abspath, filepath)
                 xfer.add_item(filepath, remote_filepath)
 
+    # Request deletes:
     if delete.items:
         _code, _reason, data = api.delete(delete)
         delete_id = data['task_id']
     else:
         delete_id = None
 
+    # Request transfers:
     if xfer.items:
         _code, _reason, data = api.transfer(xfer)
         xfer_id = data['task_id']
@@ -125,11 +138,21 @@ def perform_sync(username, token, local_name, local_path,
 
     xfer_complete = not xfer_id or wait_for_task(api, xfer_id)
     delete_complete = not delete_id or wait_for_task(api, delete_id)
-    if not (xfer_complete and delete_complete):
+    if xfer_complete and delete_complete:
+        connection.commit() # Commit local record
+    else:
         print("Some tasks did not complete")
+
+    connection.close()
 
 
 def wait_for_task(api, task_id, timeout=(2*60), step=1.5):
+    """Check the status of a requested task until `timeout` is reached or
+    status is no longer reported as "ACTIVE".
+
+    Returns True if task is no longer reported as "ACTIVE".
+
+    """
     status = 'ACTIVE'
     while timeout > 0 and status == 'ACTIVE':
         _code, _reason, data = api.task(task_id, fields="status")
@@ -139,30 +162,48 @@ def wait_for_task(api, task_id, timeout=(2*60), step=1.5):
     return status != 'ACTIVE'
 
 
-def get_remote_path(base_remote_path, base_local_path, path):
+def get_remote_path(remote_base, local_base, local_path):
     """For a given base destination path, a base origin path, and absolute
     file path, return the full appropriate destination path.
 
     """
-    norm_local_base = base_local_path.strip(os.path.sep)
-    norm_path = path.strip(os.path.sep)
+    norm_local_base = os.path.join(local_base, '')
+    if os.path.isdir(local_path):
+        norm_path = os.path.join(local_path, '')
+    else:
+        norm_path = local_path
     remote_path = norm_path.replace(norm_local_base, '', 1)
-    return os.path.join(base_remote_path, remote_path)
+    return os.path.join(remote_base, remote_path)
+
+
+def is_ancestor(parent, child):
+    """Determine whether `parent` directory path contains `child`
+    (at any level).
+
+    """
+    # Ensure parent ends with separator (to avoid similarly named siblings):
+    normalized_parent = os.path.join(parent, '')
+    return child.startswith(normalized_parent)
 
 
 class FSTable(object):
-
+    """Abstract model of filesystem paths in a SQLite database."""
     @classmethod
     def make_obj(cls, result):
+        """Map db query result to Python object appropriate to model."""
         raise NotImplementedError
 
     @staticmethod
     def _get_len(path):
         return len(path.strip(os.path.sep).split(os.path.sep))
 
+    @staticmethod
+    def get_connection(db_name):
+        return sqlite3.connect(db_name)
+
     @classmethod
     def connect(cls, db_name, table_name):
-        return cls(sqlite3.connect(db_name, isolation_level=None), table_name)
+        return cls(self.get_connection(db_name), table_name)
 
     def __init__(self, connection, table_name):
         self.connection = connection
